61b40241b4fc7d47d0fb6dd4541ce703b4f30c89
ianalexanderz <ianstewart745@gmail.com>
2021-02-25 15:31:00 -0800

    now adds users to database

---
+++ controllers/usersController.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const models = require("../models");
+
+
+router.get("/new", async (req, res) => {
+  res.render("users/new")
+});
+
+router.post('/', async (req, res) => {
+  const user = await models.user.create({
+    email: req.body.email,
+    password: req.body.password
+  })
+
+//res.cookie('userId', userId)
+  res.redirect('/')
+})
+
+/* check if that user's password matches the incoming password */
+/* router.post('/login', (res, req) => {
+  const user = await models.user.findOne 
+    ({
+      where: { email: req.body.email }
+    })
+  
+    if (user.password === req.body.password) {
+       res.cookie('userId', user.id)
+       res.redirct('/')
+    } else {
+      res.render('users/login')
+    }
+  })
+ */
+
+
+
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const app = express()
 const rowdy = require('rowdy-logger')
+const db = require('./models')
+const ejsLayouts = require("express-ejs-layouts")
 
 // middleware
 const rowdyRes = rowdy.begin(app)
 app.use(require('morgan')('tiny'))
 app.set('view engine', 'ejs')
 app.use(require('express-ejs-layouts'))
+app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: false }))
 
 // routes
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.use("/users", require("./controllers/usersController.js"))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
